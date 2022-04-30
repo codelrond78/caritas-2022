3cd867f2e7393c37bf06381270d655b57a14bd40
Lord Codelrond <codelrond@protonmail.com>
2022-04-30 13:20:25 +0000

    C2VRAYC-1 info de issue

--- api/hello.js
+++ api/hello.js
@@ -2,3 +2,4 @@ export default function handler(request, response) {
     const { name } = request.query;
     response.status(200).send(`Hello ${name}!`);
   }
+//https://codelrond-dev.atlassian.net/browse/C2VRAYC-1

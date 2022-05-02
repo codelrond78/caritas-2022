318f1d779d2de094288c25da1d201a2ff4e7e18c
Lord Codelrond <codelrond@protonmail.com>
2022-05-02 17:11:28 +0000

    added endpoints for ficha api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { expressjwt } from 'express-jwt';
 import jwks from 'jwks-rsa';
+import bodyParser from 'body-parser';
 
 var jwtCheck = expressjwt({
     secret: jwks.expressJwtSecret({
@@ -16,15 +17,42 @@ var jwtCheck = expressjwt({
 });
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 mongoose.connect(process.env.MONGODB_URI);
-const Cat = mongoose.model('Cat', { name: String });
+//const Cat = mongoose.model('Cat', { name: String });
 
+const fichaSchema = new mongoose.Schema({
+    members: [{name: String}]
+  });
+
+const Ficha = mongoose.model('Ficha', fichaSchema);
+
+app.post('/api/ficha', jwtCheck, async (req, res) => {
+    const mig = new Ficha(req.body);
+    await mig.save();
+    res.end(JSON.stringify(mig));
+})
+
+app.put('/api/ficha/:id', jwtCheck, async (req, res) => {
+    const mig = await Ficha.findByIdAndUpdate(req.params.id, req.body, 'after')
+    await mig.save();
+    res.end(JSON.stringify(mig));
+})
+
+app.get('/api/ficha/:id', jwtCheck, async (req, res) => {
+    const mig = await Ficha.findById(req.params.id)
+    res.end(JSON.stringify(mig));
+})
+
+/*
 app.post('/api/kitty', async (req, res) => {
     const kitty = new Cat({ name: 'fifi' });
     await kitty.save();
     res.end(JSON.stringify(kitty));
 });
+*/
 
 app.get('/api/g', jwtCheck, (req, res) => {
     res.end('g:)');

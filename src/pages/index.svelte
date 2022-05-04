1b7c1f099f70a33a400c761c7d0a2f542d8d9989
Lord Codelrond <codelrond@protonmail.com>
2022-05-04 09:38:09 +0000

    wip

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -21,14 +21,10 @@ $: {
 }
 
 </script>
+
+<a class="link" href="/private/ficha">Ir a ficha</a>
+
 <!--
-<a href="/ficha" class="link link-accent">Ir a ficha</a>
-
-<form use:form>
-    <input type="password" placeholder="Contraseña" name="password" class="input input-bordered w-full max-w-xs">
-    <button class="btn btn-active btn-accent">Conectar</button>
-</form>
--->
-
 <button class="btn btn-active btn-accent" on:click={() => apiError=true}>Set error!</button>
 <button class="btn btn-active btn-accent" on:click={() => apiSuccess=true}>Set success!</button>
+-->
